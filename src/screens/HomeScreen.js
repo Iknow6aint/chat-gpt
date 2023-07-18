8190bddd4da59b1399682edff37955c04d442917
Saint Agbukor <92013202+Iknow6aint@users.noreply.github.com>
2023-07-18 12:58:27 +0100

    features

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
-
+import React, { useState } from 'react'
+import { SafeAreaView, Text, View, Image } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Features from '../components/Features';
 const HomeScreen = () => {
+    const [messages, setMessages] = useState([])
     return (
-        <SafeAreaView>
-            <Text>HomeScreen</Text>
-        </SafeAreaView>
+        <View className="flex-1 bg-white">
+            <SafeAreaView className="flex-1 flex mx-5">
+                <View className="flex-row justify-center">
+                    <Image
+                        source={require('../../assets/images/bot.png')}
+                        style={{ height: hp(15), width: hp(15) }}
+                    />
+                </View>
+                {
+                    messages.length > 0 ? (
+                        <View></View>
+                    ) : (
+                        <Features />
+                    )
+                }
+            </SafeAreaView>
+        </View>
     )
 }
 
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const WelcomeScreen = () => {
     const navigation = useNavigation();
     return (
         <SafeAreaView className="flex-1 flex justify-around bg-white">
+            {/* title */}
             <View className="space-y-2">
-                <Text className="text-center font-bold text-gray-700">
+                <Text style={{ fontSize: wp(10) }} className="text-center font-bold text-gray-700">
                     Jarvis
                 </Text>
-                <Text className="text-center tracking-wider font-semibold text-gray-600">
+                <Text style={{ fontSize: wp(4) }} className="text-center tracking-wider font-semibold text-gray-600">
                     The future is here, powerd by AI.
                 </Text>
             </View>
+
             {/* assistant image */}
             <View className="flex-row justify-center">
                 <Image
                     source={require('../../assets/images/welcome.png')}
-                    className="w-72 h-72"
+                    style={{ height: wp(75), width: wp(75) }}
                 />
             </View>
+
             {/* start button */}
             <TouchableOpacity onPress={() => navigation.navigate('Home')} className="bg-emerald-600 mx-5 p-4 rounded-2xl">
-                <Text className="text-center font-bold text-white">
+                <Text style={{ fontSize: wp(6) }} className="text-center font-bold text-white">
                     Get Started
                 </Text>
             </TouchableOpacity>
